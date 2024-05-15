3309a25db8e462d2e4a08e6020562da9c7213879
yenyen31 <totoro08285@naver.com>
2024-05-15 17:48:08 +0900

    3단계 추가 수정

--- 3. 반복문/3-7(11021).py
+++ 3. 반복문/3-7(11021).py
@@ -1,5 +1,5 @@
 T = int(input())  # 테스트 케이스 개수
 
-for i in range(1, T + 1):  # Case #1부터 시작해야 하기 때문에 0 ~ T+1로 범위 설정
+for i in range(1, T + 1):  # Case #1부터 시작해야 하기 때문에 1 ~ T+1로 범위 설정
     a, b = map(int, input().split())
     print("Case #" + str(i) + ":", a + b)  # i 출력시에 str(i) 사용하는 점 주의!
--- 3. 반복문/3-8(11022).py
+++ 3. 반복문/3-8(11022).py
@@ -1,6 +1,6 @@
 T = int(input())  # 테스트 케이스 개수
 
-for i in range(1, T + 1):  # Case #1부터 시작해야 하기 때문에 0 ~ T+1로 범위 설정
+for i in range(1, T + 1):  # Case #1부터 시작해야 하기 때문에 1 ~ T+1로 범위 설정
     a, b = map(int, input().split())
     # 출력형식: "Case #x: A + B = C"
     print(f"Case #{i}: {a} + {b} = {a + b}")
